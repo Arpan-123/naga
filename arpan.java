77274f588805dc34db9dfcc6299ae2807337e96f
root <root@ip-172-31-23-215.ec2.internal>
2021-05-28 18:10:25 +0000

    java2

--- arpan.java
+++ arpan.java
@@ -1,7 +1,7 @@
 class Simple{
 public static void main (string args[]) {
 
-System.out.println("Welcome To india");
+System.out.println("Welcome To india and india is my country");
 }
 }
 ~                                                                                                                                               
